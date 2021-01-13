f036a7a2e803567079f931ad4e5408ed9727b28a
Samuel Gélineau <gelisam@simspace.com>
2021-01-13 17:05:42 -0500

    session 2: displaying a grid of Chars

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,53 @@
+{-# LANGUAGE FlexibleInstances, ViewPatterns #-}
 module Main where
 
 import Codec.Picture
 import Control.Monad.Trans.Class
 import Control.Monad.Trans.Except
+import GHC.Arr
 import Graphics.Gloss.Juicy
 import Graphics.Gloss
-import Graphics.Gloss.Data.ViewPort
 import Graphics.Gloss.Interface.IO.Interact
+
+
+type M = ExceptT String IO
+type P = (Float, Float)
+type W = (Float, Float)
+type I = (Int, Int)
+type Level = Array I Char
+
+levelSize :: I
+levelSize = (3, 3)
+
+cellSize :: P
+cellSize = (32, 32)
+
+stringLevel :: [String]
+stringLevel
+  = [ "SSG"
+    , "SGG"
+    , "SSS"
+    ]
+
+level :: Level
+level = array ((0,0), levelSize - 1)
+  [ ((x,y), c)
+  | (y, row) <- zip [0..] (reverse stringLevel)
+  , (x, c) <- zip [0..] row
+  ]
+
+drawLevel :: Level -> Picture
+drawLevel lvl
+  = translate2D (negate (totalSize / 2))
+  $ translate2D (cellSize / 2)
+  $ mconcat
+  [ translate2D p $ rectangleWire 32 32
+                 <> scale2D 0.2 (text [lvl ! (i,j)])
+  | (i,j) <- indices lvl
+  , let p = cellSize * (fromIntegral i, fromIntegral j)
+  ]
+  where
+    totalSize = cellSize * fromIntegral2D levelSize
 
 
 main :: IO ()
@@ -18,16 +59,52 @@ main = do
     Right () -> do
       pure ()
 
-type M = ExceptT String IO
-type W = (Float, Float)
-
 
 mustBeJust :: Maybe a -> M a
 mustBeJust Nothing = ExceptT $ pure $ Left "Nothing encountered"
 mustBeJust (Just a) = pure a
 
+data Dir = N | E | W | S
+  deriving (Eq, Show)
+
+isDirKey :: SpecialKey -> Maybe Dir
+isDirKey KeyRight = Just E
+isDirKey KeyLeft  = Just W
+isDirKey KeyUp    = Just N
+isDirKey KeyDown  = Just S
+isDirKey _        = Nothing
+
+unitVector :: Dir -> P
+unitVector N = (0,1)
+unitVector E = (1,0)
+unitVector W = (-1,0)
+unitVector S = (0,-1)
+
+instance Num a => Num (a,a) where
+  (x1,y1) + (x2,y2) = (x1+x2, y1+y2)
+  (x1,y1) * (x2,y2) = (x1*x2, y1*y2)
+  abs (x,y) = (abs x, abs y)
+  fromInteger x = (fromInteger x, fromInteger x)
+  negate (x,y) = (negate x, negate y)
+  signum (x,y) = (signum x, signum y)
+
+instance Fractional a => Fractional (a,a) where
+  fromRational x = (fromRational x, fromRational x)
+  recip (x,y) = (recip x, recip y)
+
+fromIntegral2D :: (Integral a, Num b)
+               => (a, a) -> (b, b)
+fromIntegral2D (x, y) = (fromIntegral x, fromIntegral y)
+
+translate2D :: (Float, Float) -> Picture -> Picture
+translate2D = uncurry translate
+
+scale2D :: (Float, Float) -> Picture -> Picture
+scale2D = uncurry scale
+
 reactWorld :: Event -> W -> W
-reactWorld (EventKey (SpecialKey KeyRight) Down _ _) (x, y) = (x+10, y)
+reactWorld (EventKey (SpecialKey (isDirKey -> Just dir)) Down _ _) w
+  = w + 10 * unitVector dir
 reactWorld _ w = w
 
 main' :: M ()
@@ -41,7 +118,7 @@ main' = do
                  <> sheets
   let displayWorld :: W -> Picture
       displayWorld (x,y) = translate x y giggles
-                        <> sheets
+                        <> drawLevel level
   let stepWorld :: Float -> W -> W
       stepWorld dt (x,y) = (x, y)
 
